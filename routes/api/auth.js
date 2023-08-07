ec20e0426ddddcccc5268d2c65887fd8b8c489d3
viktoriazakatsyura.viktoriazakatsyura <viktoriazakatsyura.viktoriazakatsyura@gmail.com>
2023-08-07 19:53:06 +0200

    hw-5

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ctrl = require("../../controllers/auth");
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.post("/login", validateBody(schemas.schema), ctrl.login);
 router.post("/logout", authenticate, ctrl.logout);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.patch("/", authenticate, validateBody(schemas.updateSchema), ctrl.updateSubscription);
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 
 module.exports = router;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,25 @@ const express = require('express');
 
 const ctrl = require("../../controllers/contacts");
 
-const {validateBody} = require('../../middlewares');
+const {validateBody, authenticate} = require('../../middlewares');
 const {schemas} = require("../../models/contacts");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll)
+router.get("/", authenticate, ctrl.getAll)
 
-router.get("/:contactId", ctrl.getById);
+router.get("/:contactId", authenticate, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/", validateBody(schemas.addSchema), ctrl.updateById);
+router.put("/", authenticate, validateBody(schemas.addSchema), ctrl.updateById);
 router.patch(
   "/:id/favorite",
+  authenticate,
   validateBody(schemas.addSchema),
   ctrl.updateFavorite
 )
 
-router.delete("/:contactId", ctrl.deleteById);
+router.delete("/:contactId", authenticate, ctrl.deleteById);
 
 module.exports = router;
